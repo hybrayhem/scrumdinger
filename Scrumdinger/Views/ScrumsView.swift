9d6d87ab9b2070b582363f188e3968c100b9c42b
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-07 02:15:28 +0300

    create scrums view

---
+++ Scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by hybrayhem.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    let scrums = DailyScrum.sampleData
+    
+    return ScrumsView(scrums: scrums)
+}
+
