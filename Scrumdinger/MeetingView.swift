cb737c386fef0300c22df2c57058be869724811b
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-07 00:59:26 +0300

    accessibility

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
+            // Header Progress
             ProgressView(value: 5, total: 15)
             HStack {
                 VStack(alignment: .leading) {
@@ -24,14 +25,22 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes") // Update this programmatically
+            
+            // Mid Circular
             Circle()
                 .strokeBorder(lineWidth: 24)
+            
+            // Footer
             HStack {
                 Text("Speaker 1 of 3")
                 Spacer()
                 Button(action: {}, label: {
                     Image(systemName: "forward.fill")
                 })
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
