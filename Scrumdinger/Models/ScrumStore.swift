d5bca143ce52390b03d433b1ddd8e4d716ea9e55
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-09 01:39:39 +0300

    load and save data

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -23,4 +23,28 @@ class ScrumStore: ObservableObject {
         )
         .appendingPathComponent(dataFileName)
     }
+    
+    func load() async throws {
+        let task = Task<[DailyScrum], Error> { // returns DailyScrum array, and throws Error
+            let file = try Self.fileURL()
+            
+            guard let data = try? Data(contentsOf: file) else {
+                return []
+            }
+            
+            let scrums = try JSONDecoder().decode([DailyScrum].self, from: data)
+            return scrums
+        }
+        
+        let scrums = try await task.value
+        self.scrums = scrums
+    }
+    
+    func save(changedScrums: [DailyScrum]) async throws {
+        try await Task {
+            let data = try JSONEncoder().encode(changedScrums)
+            let file = try Self.fileURL()
+            try data.write(to: file)
+        }.value
+    }
 }
